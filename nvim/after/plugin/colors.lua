f82eb34a8b552eb00e233dc35a6e67dd292952c6
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2023-01-12 13:33:25 -0500

    clean up more colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,14 +4,14 @@ require("noirbuddy").setup {
 		secondary = "#ffffff",
 		background = '#212733',
 
-		diagnostic_error = '#EC0034',
-		diagnostic_warning = '#ff7700',
+		diagnostic_error = '#FF3131',
+		diagnostic_warning = '#FF5733',
 		diagnostic_info = '#d5d5d5',
 		diagnostic_hint = '#f5f5f5',
 
-		diff_add = '#f5f5f5',
-		diff_change = '#737373',
-		diff_delete = '#EC0034'
+		diff_add = '#0FFF50',
+		diff_change = '#FF5733',
+		diff_delete = '#FF3131'
 	},
 }
 
@@ -20,7 +20,7 @@ function Color()
 	-- Require colorbuddy...
 	local Colors, colors, Group, groups, styles = require('colorbuddy').setup {}
 
-
+	Colors.new('background_light', '#3c475d')
 	-- vim.cmd[[
 	--     if filereadable(expand('~/.vimrc_background'))
 	--         let base16colorspace=256
@@ -36,6 +36,16 @@ function Color()
 	Group.new('ColorColumn', colors.background)
 	Group.new('Searchlight', nil, colors.secondary)
 
+	Group.new("StatusLine", colors.background, colors.none)
+	Group.new("StatusLineNC", colors.background, colors.none)
+
+	Group.new("Visual", colors.none, colors.background_light)
+
+	Group.new("PMenu", colors.primary, colors.background)
+	Group.new("PmenuSbar", colors.background, colors.secondary)
+	Group.new("PMenuSel", colors.background, colors.secondary)
+	Group.new("PmenuThumb", colors.background, colors.secondary)
+
 	Group.new('CursorColumn', colors.primary, colors.primary, styles.none)
 	Group.new('iCursor', colors.primary, colors.primary, styles.none)
 	Group.new('CursorLine', colors.primary, colors.primary, styles.none)
@@ -49,10 +59,11 @@ function Color()
 	Group.new('@constant', colors.noir_2, nil, styles.bold)
 	Group.new('@method', colors.primary, nil, styles.bold + styles.italic)
 
-	-- Link highlight groups...
-	Group.link('SignifySignAdd', groups.DiffAdd)
-	Group.link('SignifySignChange', groups.DiffChange)
-	Group.link('SignifySignDelete', groups.DiffDelete)
+	Group.new("GitGutterAdd", colors.diff_add, colors.none)
+	Group.new("GitGutterDelete", colors.diff_delete, colors.none)
+	Group.new("GitGutterChange", colors.diff_change, colors.none)
+	Group.new("GitGutterChangeDelete", colors.diff_change, colors.none)
+
 
 
 	local hl = function(thing, opts)
