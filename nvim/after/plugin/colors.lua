ce61062f71e0be3bdd8d1a7030152c0cb446f9a3
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2023-01-14 12:08:23 -0500

    update color scheme generation technique

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,49 +1,86 @@
-function string.starts(String, Start)
-	return string.sub(String, 1, string.len(Start)) == Start
-end
+function GenerateTheme(background, background_light, primary, secondary, dark)
+	local theme = {}
+	theme.background = background
+	theme.background_light = background_light
+	theme.primary = primary
+	theme.secondary = secondary
 
-local background = ""
-local primary = "#ffffff"
-local secondary = ""
-local background_light = ""
-
--- Set's the theme using the kitty theme
-for line in io.lines(os.getenv("HOME") .. "/dotfiles/kitty/theme.conf") do
-	if (string.starts(line, "background")) then
-		background = string.sub(line, string.len("background ") + 1, string.len(line))
-		print(background)
-	elseif (string.starts(line, "cursor")) then
-		secondary = string.sub(line, string.len("cursor ") + 1, string.len(line))
-		print(secondary)
-	elseif (string.starts(line, "selection_background")) then
-		background_light = string.sub(line, string.len("selection_background ") + 1, string.len(line))
-		print(background_light)
+	if dark then
+		theme.noir_0 = '#ffffff' -- `noir_0` is light for dark themes, and dark for light themes
+		theme.noir_1 = '#f5f5f5'
+		theme.noir_2 = '#d5d5d5'
+		theme.noir_3 = '#b4b4b4'
+		theme.noir_4 = '#a7a7a7'
+		theme.noir_5 = '#949494'
+		theme.noir_6 = '#737373'
+		theme.noir_7 = '#535353'
+		theme.noir_8 = '#323232'
+		theme.noir_9 = '#212121' -- `noir_9` is dark for dark themes, and light for light themes
+		vim.g.background = "dark"
+	else
+		theme.noir_0 = '#000000'
+		theme.noir_1 = '#303030'
+		theme.noir_2 = '#404040'
+		theme.noir_3 = '#545454'
+		theme.noir_4 = '#696969'
+		theme.noir_5 = '#8f8f8f'
+		theme.noir_6 = '#adadad'
+		theme.noir_7 = '#c2c2c2'
+		theme.noir_8 = '#e6e6e6'
+		theme.noir_9 = '#f5f5f5'
+		vim.g.background = "light"
 	end
+
+	return theme
 end
 
-require("noirbuddy").setup {
-	colors = {
-		primary = primary,
-		secondary = secondary,
-		background = background,
+local ayu_mirage = GenerateTheme('#212733', '#343f4c', '#ffffff', '#ffcc66', true)
+local black = GenerateTheme('#000000', '#cccccc', '#ffffff', '#ffffff', true)
+local white = GenerateTheme('#ffffff', '#cccccc', '#000000', '#000000', false)
 
-		diagnostic_error = '#FF3131',
-		diagnostic_warning = '#FF5733',
-		diagnostic_info = '#d5d5d5',
-		diagnostic_hint = '#f5f5f5',
+function Color(theme)
+	local t = ayu_mirage
+	if theme == "ayu_mirage" then
+		t = ayu_mirage
+	elseif theme == "black" then
+		t = black
+	elseif theme == "white" then
+		t = white
+	end
 
-		diff_add = '#0FFF50',
-		diff_change = '#FF5733',
-		diff_delete = '#FF3131'
-	},
-}
+	require("noirbuddy").setup {
+		colors = {
+			primary = t.primary,
+			secondary = t.secondary,
+			background = t.background,
 
+			noir_0 = t.noir_0,
+			noir_1 = t.noir_1,
+			noir_2 = t.noir_2,
+			noir_3 = t.noir_3,
+			noir_4 = t.noir_4,
+			noir_5 = t.noir_5,
+			noir_6 = t.noir_6,
+			noir_7 = t.noir_7,
+			noir_8 = t.noir_8,
+			noir_9 = t.noir_9,
 
-function Color()
+			diagnostic_error = '#FF3131',
+			diagnostic_warning = '#FF5733',
+			diagnostic_info = '#d5d5d5',
+			diagnostic_hint = '#f5f5f5',
+
+			diff_add = '#0FFF50',
+			diff_change = '#FF5733',
+			diff_delete = '#FF3131'
+		}
+	}
+
 	-- Require colorbuddy...
 	local Colors, colors, Group, groups, styles = require('colorbuddy').setup {}
 
-	Colors.new('background_light', background_light)
+	Colors.new('background_light', t.background_light)
+
 	-- vim.cmd[[
 	--     if filereadable(expand('~/.vimrc_background'))
 	--         let base16colorspace=256
