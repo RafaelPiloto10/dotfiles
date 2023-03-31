7b04efe367d4ca0621514ccb5bb3b07025eb13eb
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2023-03-30 23:43:34 -0400

    add netwr keybinds

--- nvim/after/plugin/keymap/init.lua
+++ nvim/after/plugin/keymap/init.lua
@@ -21,5 +21,6 @@ vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<C-s>', '<C-w>w')
+vim.keymap.set('n', '<leader>fe', ':Explore<CR>')
 
 
